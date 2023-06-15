43800119c71abda0af109bdfe7d21c447d87d169
Nelitha Vindinu Priyawansha <nelithavindinu7@gmail.com>
2023-06-15 20:26:20 +0530

    shop and product page done

--- home/shop.js
+++ home/shop.js
@@ -1,28 +1,63 @@
-let minValue = document.getElementById("min-value");
-let maxValue = document.getElementById("max-value");
+const menu = document.querySelector(".menu");
+const navOpen = document.querySelector(".hamburger");
+const navClose = document.querySelector(".close");
 
-function validateRange(minPrice, maxPrice) {
-  if (minPrice > maxPrice) {
-
-    // Swap to Values
-    let tempValue = maxPrice;
-    maxPrice = minPrice;
-    minPrice = tempValue;
+const navLeft = menu.getBoundingClientRect().left;
+navOpen.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.add("show");
+    document.body.classList.add("show");
+    navBar.classList.add("show");
   }
+});
 
-  minValue.innerHTML = "$" + minPrice;
-  maxValue.innerHTML = "$" + maxPrice;
-}
+navClose.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+    navBar.classList.remove("show");
+  }
+});
 
-const inputElements = document.querySelectorAll("input");
+// Fixed Nav
+const navBar = document.querySelector(".nav");
+const navHeight = navBar.getBoundingClientRect().height;
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix-nav");
+  } else {
+    navBar.classList.remove("fix-nav");
+  }
+});
 
-inputElements.forEach((element) => {
-  element.addEventListener("change", (e) => {
-    let minPrice = parseInt(inputElements[0].value);
-    let maxPrice = parseInt(inputElements[1].value);
+// Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+links.map(link => {
+  if (!link) return;
+  link.addEventListener("click", e => {
+    e.preventDefault();
 
-    validateRange(minPrice, maxPrice);
+    const id = e.target.getAttribute("href").slice(1);
+
+    const element = document.getElementById(id);
+    const fixNav = navBar.classList.contains("fix-nav");
+    let position = element.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    navBar.classList.remove("show");
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
   });
 });
 
-validateRange(inputElements[0].value, inputElements[1].value);
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
+gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".hero-content h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
